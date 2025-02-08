6275c0d7261955f291bb17a61bb328215ab8b0b1
Crownedtechie <nenchi00@gmail.com>
2025-02-08 01:43:25 +0100

    --setup:: add types for typography component

--- src/components/typography/types.ts
+++ src/components/typography/types.ts
@@ -1,3 +1,16 @@
+import { ReactNode } from "react";
+
 export type TypographyElements = "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "span" | "pre" | "em" | "strong" | "mark" | "del" | "ins" | "sub" | "sup";
 
- 
+export type TypographyColors = "beige500" | "beige100" | "grey900" | "grey500" | "grey300" | "grey100" | "green" | "yellow" | "cyan" | "navy" | "red" | "purple-secondary" | "purple-tertiary" | "turquoise" | "brown" | "magenta" | "blue" | "navy-grey" | "army-green" | "gold" | "orange";
+
+export type TypographyFontWeight = "regular" | "medium" | "bold";
+
+
+export interface TypographyProps extends React.HTMLAttributes<HTMLOrSVGElement> {
+ as?: TypographyElements;
+ children: ReactNode;
+ color?: TypographyColors;
+ fontWeight?: TypographyFontWeight;
+ customClass?: string;
+}
