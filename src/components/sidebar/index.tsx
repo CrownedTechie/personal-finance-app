cf1ff7707ba0fddb37f140ec43fe90af6338a064
Crownedtechie <nenchi00@gmail.com>
2025-02-20 17:35:14 +0100

    --style:: style side bar and the tabs active state

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,6 +1,8 @@
 import clsx from "clsx";
 import logoLarge from "../../assets/logo-large.svg";
 import { Tabs } from "../tabs";
+import { Typography } from "../typography";
+import { PiArrowFatLinesLeftFill } from "react-icons/pi";
 
 type Props = {
  
@@ -9,19 +11,30 @@ export const Sidebar = ({}: Props) => {
  return ( 
   <aside 
    className={clsx(
-    "fixed left-0 top-0 h-screen w-64 transition-all duration-300 z-50 bg-grey900 text-grey300 font-bold rounded-br-200 rounded-tr-200 flex flex-col justify-between"
+    "fixed left-0 top-0 h-screen w-64 transition-all duration-300 z-50 bg-grey900 text-grey300 rounded-br-200 rounded-tr-200 flex flex-col gap-300"
    )}
   >
-   <header>
+   <header className="px-400 py-500 rounded-tr-200">
     <img src={logoLarge} alt="Finance logo" />
    </header>
 
-   <nav className="flex flex-col">
+   <nav className="flex flex-col pr-300 gap-50">
     <Tabs />
    </nav>
 
-   <footer>
-    <button>Minimize menu</button>
+   <footer className="fixed bottom-0 left-0 pb-300 px-400">
+    <button>
+     <Typography 
+      fontWeight="bold" 
+      fontSize="md" 
+      color="grey300"
+      customClass="flex items-center gap-200 hover:text-grey100 hover:cursor-pointer"
+     >
+     <PiArrowFatLinesLeftFill className="text-[1.5rem] " />
+      Minimize menu
+     </Typography>
+
+    </button>
    </footer>
    
   </aside>
--- src/components/tabs/index.tsx
+++ src/components/tabs/index.tsx
@@ -1,25 +1,54 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useSearchParams } from "react-router-dom";
 import { Typography } from "../typography";
-import { PiArrowsDownUpFill, PiHouseFill, PiChartDonutFill, PiReceiptFill } from "react-icons/pi";
+import { PiArrowsDownUpFill, PiHouseFill, PiChartDonutFill, PiReceiptFill, PiTipJarFill } from "react-icons/pi";
+import clsx from "clsx";
+import { useEffect } from "react";
 
 const tabs = [
- {name: "Overview", location:"#", icon: <PiHouseFill />},
- {name: "Transactions", location:"#", icon: <PiArrowsDownUpFill />},
- {name: "Budgets", location:"#", icon: <PiChartDonutFill />},
- {name: "Pots", location:"#"},
- {name: "Recurring bills", location:"#", icon: <PiReceiptFill />},
+ {name: "Overview", location:"", icon: <PiHouseFill />},
+ {name: "Transactions", location:"", icon: <PiArrowsDownUpFill />},
+ {name: "Budgets", location:"", icon: <PiChartDonutFill />},
+ {name: "Pots", location:"", icon: <PiTipJarFill />},
+ {name: "Recurring bills", location:"", icon: <PiReceiptFill />},
 ];
 
 
 export const Tabs = () => {
+ const [searchParams, setSearchParams] = useSearchParams();
+ const activeTab = searchParams.get("tab") || "Overview";
+
+ useEffect(() => {
+    const tab = searchParams.get("tab");
+    if (!tab) {
+      setSearchParams({ tab: "Overview" }, { replace: true });
+    }
+  }, [searchParams.get("tab"), setSearchParams]);
+ 
  return (
   <>
    {tabs.map(tab => (
     <NavLink 
      key={tab.name}
-     to={tab.location}
+     to={`?tab=${tab.name}` || tab.location}
+     className={({isActive}) => 
+      clsx(
+       `flex items-center rounded-tr-150 rounded-br-150 gap-200 px-400 py-200 hover:*:text-beige100`,
+        activeTab === tab.name && "bg-beige100 border-l-4 border-l-green "
+      )
+     }
     >
-     <Typography color="grey300">
+     <Typography 
+      color={activeTab === tab.name ? "green" : "grey300"}
+      fontWeight="bold"
+      customClass="text-[1.5rem]"
+     >
+      {tab.icon}
+     </Typography>
+     <Typography 
+      color={activeTab === tab.name ? "grey900" : "grey300"}
+      fontSize="md"
+      fontWeight="bold"
+     >
       {tab.name}
      </Typography>
     </NavLink>
