b0969f367a902ed9e4391a7f05c917b45ee27d29
Crownedtechie <nenchi00@gmail.com>
2025-03-09 10:21:11 +0100

    --feat:: implement more options design and make it render on button click

--- src/components/cards/PotsCard.tsx
+++ src/components/cards/PotsCard.tsx
@@ -4,6 +4,7 @@ import { ContentHeader } from "../contentHeader";
 import { CardWrapper } from "./CardWrapper";
 import { Typography } from "../typography";
 import { formattedAmount } from "@/utils/formatAmount";
+import { useState } from "react";
 
 interface IPotsCardProps {
  title: string;
@@ -13,13 +14,18 @@ interface IPotsCardProps {
 };
 
 export const PotsCard = ({title, itemColor, totalSaved, targetAmount}: IPotsCardProps) => {
- const formattedTotalSaved = formattedAmount(totalSaved);
- const formattedTargetAmount = formattedAmount(targetAmount);
- const progressPercentage = ((totalSaved / targetAmount) * 100);
+  const [openMoreOptions, setOpenMoreOptions] = useState<boolean>(false);
+  const formattedTotalSaved = formattedAmount(totalSaved);
+  const formattedTargetAmount = formattedAmount(targetAmount);
+  const progressPercentage = ((totalSaved / targetAmount) * 100);
+
+  const toggleMoreOptions = () => {
+    setOpenMoreOptions(prevState => !prevState);
+  };
 
  return ( 
   <CardWrapper>
-   <div className="flex flex-col justify-center gap-400">
+   <div className="flex flex-col justify-center gap-400 relative">
     <ContentHeader
       as="h2"
       fontWeight="bold"
@@ -28,6 +34,7 @@ export const PotsCard = ({title, itemColor, totalSaved, targetAmount}: IPotsCard
       buttonGroup={
        <Button 
         className="outline-none text-grey300 cursor-pointer"
+        onClick={toggleMoreOptions}
        >
         <PiDotsThreeOutlineFill className="size-200" />
        </Button>
@@ -76,13 +83,24 @@ export const PotsCard = ({title, itemColor, totalSaved, targetAmount}: IPotsCard
       </div>
      </div>
 
-     {/* Button groups */}
-     <div className="grid grid-cols-2 gap-200">
+      {/* Button groups */}
+      <div className="grid grid-cols-2 gap-200">
       <Button variant="secondary">+ Add Money</Button>
       <Button variant="secondary">Withdraw</Button>
-     </div>
+      </div>
 
+      {/* More options */}
+      {openMoreOptions && (
+      <ul className="absolute right-1 top-8 px-250 py-150 bg-white shadow-[0px_4px_24px_0px_rgba(0,0,0,0.25)] flex flex-col justify-between gap-150 rounded-100 max-h-[18.75rem] z-50">
+        <li className="cursor-pointer"><Typography>Edit Pot</Typography></li>
+        <hr className="text-grey100" />
+        <li className="cursor-pointer"><Typography color="red">Delete Pot</Typography></li>
+      </ul>
+      )}
+      
    </div>
+
+   
   </CardWrapper>
  );
 }
